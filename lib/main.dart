f44bf1efb736fba0e1740affa91baf11f41e281b
kareemtoson <kareemtoson1@gmail.com>
2024-11-17 15:54:30 +0200

    Sign up done ui and apis

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,3 @@ void main() async {
     appRouter: AppRoutes(),
   ));
 }
-/* final dio = DioFactory.getDio();
-
-  // Create an instance of ApiServices
-  final apiService = ApiServices(dio);
-
-  // Test the API
-  try {
-    // Create the request object
-    final signupRequest = SignupRequestBody(
-      username: "kareemo",
-      email: "toson289782@gmail.com",
-      password: "01226211829Kk@#",
-      phoneNumber: "0122621898",
-    );
-
-    // Call the sign-up API
-    final response = await apiService.signUp(signupRequest);
-
-    // Print the response
-    print("Response: ${response.message}");
-  } catch (e) {
-    print("Error: $e");
-  } */
